3ab93687bce613e3a61ba32734e8086bba1d30ef
scamgi <scampini.giacomo@gmail.com>
2020-05-23 23:47:51 +0200

    gulp successfully installed

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var del = require('del');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+var server = browserSync.create();
+
+function clean() {
+  return del(['app/css/*']);
+};
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: './app'
+    }
+  });
+  done();
+}
+
+function buildSass() {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+}
+
+function watch() {
+  return gulp.watch('./app/scss/*.scss', gulp.series(buildSass, reload));
+}
+
+var dev = gulp.series(clean, buildSass, serve, watch);
+module.exports.default = dev;
