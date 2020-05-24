d672b0d3f512d16b6522b8721dc7b14eec37af88
scamgi <scampini.giacomo@gmail.com>
2020-05-24 12:52:42 +0200

    sass gulp task added

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,14 @@ var del = require('del');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-
+var rename = require('gulp-rename');
 var server = browserSync.create();
+
+var patterns = {
+  base: './app',
+  sass: './app/scss/*.scss',
+  destSass: './app/css'
+};
 
 function clean() {
   return del(['app/css/*']);
@@ -17,21 +23,29 @@ function reload(done) {
 function serve(done) {
   server.init({
     server: {
-      baseDir: './app'
+      baseDir: patterns.base
     }
   });
   done();
 }
 
-function buildSass() {
-  return gulp.src('app/scss/*.scss')
+function expandedSass() {
+  return gulp.src(patterns.sass)
     .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('./app/css'));
+    .pipe(gulp.dest(patterns.destSass));
 }
+
+function compressedSass() {
+  return gulp.src(patterns.sass)
+    .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+    .pipe(rename({extname:'.min.css'}))
+    .pipe(gulp.dest(patterns.destSass));
+}
+
 
 function watch() {
-  return gulp.watch('./app/scss/*.scss', gulp.series(buildSass, reload));
+  return gulp.watch('./app/scss/*.scss', gulp.series(expandedSass, reload));
 }
 
-var dev = gulp.series(clean, buildSass, serve, watch);
-module.exports.default = dev;
+module.exports.default = gulp.series(clean, expandedSass, serve, watch);
+module.exports.sass = gulp.parallel(expandedSass, compressedSass);
